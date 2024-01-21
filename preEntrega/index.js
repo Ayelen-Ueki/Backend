187fb51664a0cc41611b7726d3f82bf2777ebc97
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2024-01-21 13:48:01 -0300

    Pre Entrega

--- preEntrega/index.js
+++ preEntrega/index.js
@@ -6,6 +6,7 @@ import {__dirname} from './paths.js '
 const PORT = 8000
 const app = express()
 
+app.use('/api/carts', routerCart)
 app.use('/api/products', routerProd)
 app.use('/static', express.static(path.join(__dirname, '/public')))
 
