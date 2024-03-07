9b190a3f76d1c56e8c7192fc1dabc120dd788c9c
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2024-03-07 13:30:52 -0300

    Desafio 5

--- desafio5/routes/auth.routes.js
+++ desafio5/routes/auth.routes.js
@@ -5,7 +5,22 @@ router.post('/register', (req, res)=>{
     newUser.id = Math.random()
     users.push(newUser)
 
-    res.redirect('view/login-view')
+    res.redirect('/views/login-view')
+})
+
+router.post('/login', (req, res)=>{
+    let newUser=req.body
+    let userfound=users.find(user=>{
+        return user.useremail == newUser.useremail && user.password == newUser.password
+    })
+    if(userfound){
+        req.session.user = newUser.user
+        req.session.password = newUser.password
+
+        res.redirect('/views/profile-view')
+        return
+    }
+    res.send("Usuario o contraseña incorrectos")
 })
 
 router.get('/user', (req,res)=>{
